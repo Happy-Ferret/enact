6dc579ada8d05715829c04d93d941b1e6e9ae1ae
Ryan Duffy <ryan.duffy@lge.com>
2017-09-06 11:16:46 -0500

    add ui/Remeasurable contextTypes to moonstone/internal/Pure
    Enyo-DCO-1.1-Signed-off-by: Ryan Duffy (ryan.duffy@lge.com)

--- packages/moonstone/internal/Pure/Pure.js
+++ packages/moonstone/internal/Pure/Pure.js
@@ -1,11 +1,13 @@
 import hoc from '@enact/core/hoc';
 import {contextTypes as i18nContextTypes} from '@enact/i18n/I18nDecorator';
+import {contextTypes as remeasureContextTypes} from '@enact/ui/Remeasurable';
 import {contextTypes as skinContextTypes} from '@enact/ui/Skinnable';
 import UiPure from '@enact/ui/internal/Pure';
 
 const defaultConfig = {
 	contextTypes: [
 		i18nContextTypes,
+		remeasureContextTypes,
 		skinContextTypes
 	]
 };
